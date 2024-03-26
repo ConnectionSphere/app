62b25d9f1fe83582906ac1c0d814e676c399e56f
leandrosardi <leandro.sardi@expandedventure.com>
2024-03-26 15:41:09 -0300

    leandrosardi/my.saas#5

--- deployment/install-webserver.rb
+++ deployment/install-webserver.rb
@@ -4,17 +4,17 @@ BlackStack::Deployer::add_routine({
   :commands => [{
     :command => '
       # remove old code folder
-      rmdir -rf ~/code/connectionshere/app >>~/connectionsphere.app.output 2>&1
+      rmdir -rf ~/code/connectionsphere/app >>~/connectionsphere.app.output 2>&1
 
       # create code folder
-      mkdir -p ~/code/connectionshere >>~/connectionsphere.app.output 2>&1
-      mkdir -p ~/code/connectionshere >>~/connectionsphere.app.output 2>&1
+      mkdir -p ~/code/connectionsphere >>~/connectionsphere.app.output 2>&1
+      mkdir -p ~/code/connectionsphere >>~/connectionsphere.app.output 2>&1
 
       # clone the project
-      git clone https://%git_username%:%git_password%@github.com/connectionsphere/app ~/code/connectionshere >>~/connectionsphere.app.output 2>&1
+      git clone https://%git_username%:%git_password%@github.com/connectionsphere/app ~/code/connectionsphere >>~/connectionsphere.app.output 2>&1
 
       # pull the last version of the source code
-      cd ~/code/connectionshere
+      cd ~/code/connectionsphere
       git config --global credential.helper store >>~/connectionsphere.app.output 2>&1
       git fetch --all >>~/connectionsphere.app.output 2>&1
       git reset --hard origin/%git_branch% >>~/connectionsphere.app.output 2>&1
@@ -25,60 +25,60 @@ BlackStack::Deployer::add_routine({
       git reset --hard origin/%git_branch% >>~/connectionsphere.app.output 2>&1
 
       # update extension i2p
-      cd ~/code/connectionshere/extensions >>~/connectionsphere.app.output 2>&1
-      git clone https://github.com/leandrosardi/i2p ~/code/connectionshere/extensions/i2p >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions >>~/connectionsphere.app.output 2>&1
+      git clone https://github.com/leandrosardi/i2p ~/code/connectionsphere/extensions/i2p >>~/connectionsphere.app.output 2>&1
       git config --global credential.helper store >>~/connectionsphere.app.output 2>&1
-      cd ~/code/connectionshere/extensions/i2p >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions/i2p >>~/connectionsphere.app.output 2>&1
       git fetch --all >>~/connectionsphere.app.output 2>&1
       git reset --hard origin/master >>~/connectionsphere.app.output 2>&1
 
       # update extension monitoring
-      cd ~/code/connectionshere/extensions >>~/connectionsphere.app.output 2>&1
-      git clone https://github.com/leandrosardi/monitoring ~/code/connectionshere/extensions/monitoring >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions >>~/connectionsphere.app.output 2>&1
+      git clone https://github.com/leandrosardi/monitoring ~/code/connectionsphere/extensions/monitoring >>~/connectionsphere.app.output 2>&1
       git config --global credential.helper store >>~/connectionsphere.app.output 2>&1
-      cd ~/code/connectionshere/extensions/monitoring >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions/monitoring >>~/connectionsphere.app.output 2>&1
       git fetch --all >>~/connectionsphere.app.output 2>&1
       git reset --hard origin/main >>~/connectionsphere.app.output 2>&1
 
       # update extension content
-      cd ~/code/connectionshere/extensions >>~/connectionsphere.app.output 2>&1
-      git clone https://github.com/leandrosardi/content ~/code/connectionshere/extensions/content >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions >>~/connectionsphere.app.output 2>&1
+      git clone https://github.com/leandrosardi/content ~/code/connectionsphere/extensions/content >>~/connectionsphere.app.output 2>&1
       git config --global credential.helper store >>~/connectionsphere.app.output 2>&1
-      cd ~/code/connectionshere/extensions/content >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions/content >>~/connectionsphere.app.output 2>&1
       git fetch --all >>~/connectionsphere.app.output 2>&1
       git reset --hard origin/main >>~/connectionsphere.app.output 2>&1
 
       # update extension selectrowsjs
-      cd ~/code/connectionshere/extensions >>~/connectionsphere.app.output 2>&1
-      git clone https://github.com/leandrosardi/selectrowsjs ~/code/connectionshere/extensions/selectrowsjs >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions >>~/connectionsphere.app.output 2>&1
+      git clone https://github.com/leandrosardi/selectrowsjs ~/code/connectionsphere/extensions/selectrowsjs >>~/connectionsphere.app.output 2>&1
       git config --global credential.helper store >>~/connectionsphere.app.output 2>&1
-      cd ~/code/connectionshere/extensions/selectrowsjs >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions/selectrowsjs >>~/connectionsphere.app.output 2>&1
       git fetch --all >>~/connectionsphere.app.output 2>&1
       git reset --hard origin/main >>~/connectionsphere.app.output 2>&1
 
       # update extension filtersjs
-      cd ~/code/connectionshere/extensions >>~/connectionsphere.app.output 2>&1
-      git clone https://github.com/leandrosardi/filtersjs ~/code/connectionshere/extensions/filtersjs >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions >>~/connectionsphere.app.output 2>&1
+      git clone https://github.com/leandrosardi/filtersjs ~/code/connectionsphere/extensions/filtersjs >>~/connectionsphere.app.output 2>&1
       git config --global credential.helper store >>~/connectionsphere.app.output 2>&1
-      cd ~/code/connectionshere/extensions/filtersjs >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions/filtersjs >>~/connectionsphere.app.output 2>&1
       git fetch --all >>~/connectionsphere.app.output 2>&1
       git reset --hard origin/main >>~/connectionsphere.app.output 2>&1
 
       # update extension templatesjs
-      cd ~/code/connectionshere/extensions >>~/connectionsphere.app.output 2>&1
-      git clone https://github.com/leandrosardi/templatesjs ~/code/connectionshere/extensions/templatesjs >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions >>~/connectionsphere.app.output 2>&1
+      git clone https://github.com/leandrosardi/templatesjs ~/code/connectionsphere/extensions/templatesjs >>~/connectionsphere.app.output 2>&1
       git config --global credential.helper store >>~/connectionsphere.app.output 2>&1
-      cd ~/code/connectionshere/extensions/templatesjs >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere/extensions/templatesjs >>~/connectionsphere.app.output 2>&1
       git fetch --all >>~/connectionsphere.app.output 2>&1
       git reset --hard origin/main >>~/connectionsphere.app.output 2>&1
 
       # upload configuration file
-      cd ~/code/connectionshere >>~/connectionsphere.app.output 2>&1
+      cd ~/code/connectionsphere >>~/connectionsphere.app.output 2>&1
       [ -f ./config.rb ] && mv ./config.rb ./config.%timestamp%.rb >>~/connectionsphere.app.output 2>&1
       echo "%config_rb_content%" > ./config.rb
 
       # update gems
-      #cd ~/code/connectionshere >>~/connectionsphere.app.output 2>&1
+      #cd ~/code/connectionsphere >>~/connectionsphere.app.output 2>&1
       #bundler update >>~/connectionsphere.app.output 2>&1
     ',
   }],
