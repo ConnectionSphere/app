c0d1b6bf022a6337d6a56e8f0761b46dde5e3d8a
leandrosardi <leandro.sardi@expandedventure.com>
2024-03-26 17:22:36 -0300

    fix minor glitches in form submission + add actions + add unibox

--- start.proc.sh
+++ start.proc.sh
@@ -3,7 +3,7 @@ ps ax | grep "code/connectionsphere" | grep -v postgres | grep -v grep | cut -b1
 # start webserver
 export RUBYLIB=~/code/connectionsphere
 cd ~/code/connectionsphere
-#ruby $HOME/code/connectionsphere/p/actions.rb > /dev/null 2>&1 &
+ruby $HOME/code/connectionsphere/p/actions.rb > /dev/null 2>&1 &
 ruby $HOME/code/connectionsphere/p/deletion.rb > /dev/null 2>&1 &
 ruby $HOME/code/connectionsphere/p/delivery.rb > /dev/null 2>&1 &
 ruby $HOME/code/connectionsphere/p/export.rb > /dev/null 2>&1 &
@@ -11,7 +11,7 @@ ruby $HOME/code/connectionsphere/p/leads.upload.import.rb > /dev/null 2>&1 &
 ruby $HOME/code/connectionsphere/p/leads.upload.ingest.rb > /dev/null 2>&1 &
 ruby $HOME/code/connectionsphere/p/planner.rb > /dev/null 2>&1 &
 ruby $HOME/code/connectionsphere/p/timeline.rb > /dev/null 2>&1 &
-#ruby $HOME/code/connectionsphere/p/receive.rb > /dev/null 2>&1 &
+ruby $HOME/code/connectionsphere/p/receive.rb > /dev/null 2>&1 &
 ruby $HOME/code/connectionsphere/extensions/i2p/p/ipn.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
 #ruby $HOME/code/connectionsphere/extensions/i2p/p/baddebt.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
 #ruby $HOME/code/connectionsphere/extensions/i2p/p/expire.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
