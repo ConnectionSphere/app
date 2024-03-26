49ad0cd55618ed52cdf1837e37cc77fc02a5e7ea
leandrosardi <leandro.sardi@expandedventure.com>
2024-03-26 15:51:13 -0300

    leandrosardi/my.saas#5

--- start.proc.sh
+++ start.proc.sh
@@ -1,17 +1,17 @@
 # kill all micro.data processed except postgress
-ps ax | grep "code/connectionshere" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9 >/dev/null 2>&1;
+ps ax | grep "code/connectionsphere" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9 >/dev/null 2>&1;
 # start webserver
-export RUBYLIB=~/code/connectionshere
-cd ~/code/connectionshere
-#ruby $HOME/code/connectionshere/p/actions.rb > /dev/null 2>&1 &
-ruby $HOME/code/connectionshere/p/deletion.rb > /dev/null 2>&1 &
-ruby $HOME/code/connectionshere/p/delivery.rb > /dev/null 2>&1 &
-ruby $HOME/code/connectionshere/p/export.rb > /dev/null 2>&1 &
-ruby $HOME/code/connectionshere/p/leads.upload.import.rb > /dev/null 2>&1 &
-ruby $HOME/code/connectionshere/p/leads.upload.ingest.rb > /dev/null 2>&1 &
-ruby $HOME/code/connectionshere/p/planner.rb > /dev/null 2>&1 &
-ruby $HOME/code/connectionshere/p/timeline.rb > /dev/null 2>&1 &
-#ruby $HOME/code/connectionshere/p/receive.rb > /dev/null 2>&1 &
-ruby $HOME/code/connectionshere/extensions/i2p/p/ipn.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
-#ruby $HOME/code/connectionshere/extensions/i2p/p/baddebt.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
-#ruby $HOME/code/connectionshere/extensions/i2p/p/expire.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
+export RUBYLIB=~/code/connectionsphere
+cd ~/code/connectionsphere
+#ruby $HOME/code/connectionsphere/p/actions.rb > /dev/null 2>&1 &
+ruby $HOME/code/connectionsphere/p/deletion.rb > /dev/null 2>&1 &
+ruby $HOME/code/connectionsphere/p/delivery.rb > /dev/null 2>&1 &
+ruby $HOME/code/connectionsphere/p/export.rb > /dev/null 2>&1 &
+ruby $HOME/code/connectionsphere/p/leads.upload.import.rb > /dev/null 2>&1 &
+ruby $HOME/code/connectionsphere/p/leads.upload.ingest.rb > /dev/null 2>&1 &
+ruby $HOME/code/connectionsphere/p/planner.rb > /dev/null 2>&1 &
+ruby $HOME/code/connectionsphere/p/timeline.rb > /dev/null 2>&1 &
+#ruby $HOME/code/connectionsphere/p/receive.rb > /dev/null 2>&1 &
+ruby $HOME/code/connectionsphere/extensions/i2p/p/ipn.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
+#ruby $HOME/code/connectionsphere/extensions/i2p/p/baddebt.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
+#ruby $HOME/code/connectionsphere/extensions/i2p/p/expire.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
--- start.web.sh
+++ start.web.sh
@@ -1,6 +1,6 @@
 # kill all micro.data processed except postgress
-ps ax | grep "code/connectionshere" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9 >/dev/null 2>&1;
+ps ax | grep "code/connectionsphere" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9 >/dev/null 2>&1;
 # start webserver
-export RUBYLIB=~/code/connectionshere
+export RUBYLIB=~/code/connectionsphere
 cd ~/code/connectionsphere
 ruby $HOME/code/connectionsphere/app.rb port=3000 config=./config.rb > /dev/null 2>&1 &
